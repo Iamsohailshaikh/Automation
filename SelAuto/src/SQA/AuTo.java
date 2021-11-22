e89ee2914c90fd19efd59c874c6a2bcf42bec7b4
Iamsohailshaikh <94803085+Iamsohailshaikh@users.noreply.github.com>
2021-11-22 20:39:53 +0530

    Update AuTo.java

--- SelAuto/src/SQA/AuTo.java
+++ SelAuto/src/SQA/AuTo.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;  
 
 public class AuTo {
-	///Hello world   
+	  
     public static void main(String[] args) {  
         
     // declaration and instantiation of objects/variables  
